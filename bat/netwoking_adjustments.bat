872cc49fc45ad944766e2c27e7b9ffaddeb964ea
Gonchik Tsymzhitov <gonchik.tsymzhitov@atlassiancommunity.com>
2024-08-14 10:49:53 +0300

    Update netwoking_adjustments.bat

--- bat/netwoking_adjustments.bat
+++ bat/netwoking_adjustments.bat
@@ -12,20 +12,20 @@ netsh int ipv6 set dynamicport udp start=4000 num=61530
 
 :: Enable TCP Window Scaling
 echo Enabling TCP Window Scaling
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "Tcp1323Opts" /t REG_DWORD /d "1" /f >> APB_Log.txt
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "Tcp1323Opts" /t REG_DWORD /d "1" /f
 timeout /t 1 /nobreak > NUL
 
 :: Set TcpMaxDupAcks
 echo Setting TcpMaxDupAcks to 2
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "TcpMaxDupAcks" /t REG_DWORD /d "2" /f >> APB_Log.txt
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "TcpMaxDupAcks" /t REG_DWORD /d "2" /f
 timeout /t 1 /nobreak > NUL
 
 :: Disable SackOpts
 echo Disabling TCP Selective ACKs
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "SackOpts" /t REG_DWORD /d "0" /f >> APB_Log.txt
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "SackOpts" /t REG_DWORD /d "0" /f
 timeout /t 1 /nobreak > NUL
 
 :: Decrease Time to Wait in "TIME_WAIT" State
 echo Decreasing Timed Wait Delay
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "TcpTimedWaitDelay" /t REG_DWORD /d "30" /f >> APB_Log.txt
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "TcpTimedWaitDelay" /t REG_DWORD /d "30" /f
 timeout /t 1 /nobreak > NUL
